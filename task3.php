f5f699c97809d1f442fa2bda1e5d00e1e4a01bf5
FUMIHIKO ARAI <hbb.arararara@gmail.com>
2019-04-29 16:34:26 +0900

    一時保存

---
+++ task3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8" />
+  <title>js中級タスク1</title>
+  <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<!-- ヘッダー -->
+<?php require('header.php') ?>
+
+<!-- メイン -->
+
+
+<!-- footer -->
+<?php
+require('footer.php');
+?>
+
+</html>
