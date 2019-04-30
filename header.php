e2df8c8714ff7ba84afcc1700bce571362f9711d
FUMIHIKO ARAI <hbb.arararara@gmail.com>
2019-04-30 16:18:44 +0900

    txtファイル変換

--- header.php
+++ header.php
@@ -13,11 +13,11 @@
       <div class="header-menu">
         <nav id="menu" class="menu">
           <ul>
-            <li><a href="#">足し算</a></li>
-            <li><a href="#">月別日数検索</a></li>
-            <li><a href="#">画像切り替え</a></li>
-            <li><a href="#">txtファイル変換</a></li>
-            <li><a href="#">ウェザーニュース</a></li>
+            <li><a href="task1.php">足し算</a></li>
+            <li><a href="task2.php">月別日数検索</a></li>
+            <li><a href="task3.php">画像切り替え</a></li>
+            <li><a href="task4.php">txtファイル変換</a></li>
+            <li><a href="task5.php">ウェザーニュース</a></li>
           </ul>
         </nav>
       </div>
