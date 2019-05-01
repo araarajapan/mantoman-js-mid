cb90cfcef2349e4bce200a8a1a920e886d30bba7
FUMIHIKO ARAI <hbb.arararara@gmail.com>
2019-05-01 12:43:30 +0900

    ウェザーニュースver1.1

--- task5.php
+++ task5.php
@@ -12,15 +12,17 @@
 
 <!-- メイン -->
 
-<select name="" id="js-city-select">
-  <option value="130010">東京</option>
-  <option value="140010">横浜</option>
-  <option value="120010">千葉</option>
-  <option value="110010">さいたま</option>
-  <option value="100010">前橋</option>
-  <option value="090010">宇都宮</option>
-  <option value="080010">水戸</option>
-</select>
+<div class="main">
+  <select name="" id="js-city-select">
+    <option value="130010">東京</option>
+    <option value="140010">横浜</option>
+    <option value="120010">千葉</option>
+    <option value="110010">さいたま</option>
+    <option value="100010">前橋</option>
+    <option value="090010">宇都宮</option>
+    <option value="080010">水戸</option>
+  </select>
+</div>
 
 
 <!-- footer -->
